f4b12d7403adab710321df7a855776b530563b44
zaza121 <hamza.yaya121@gmail.com>
2023-10-10 09:09:44 +0100

    [FIX] Cannot import data : no name in "resource.resource"

--- addons/opsol_topnett/models/hr_employee.py
+++ addons/opsol_topnett/models/hr_employee.py
@@ -5,10 +5,17 @@ from odoo import api, fields, models
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
-    name = fields.Char(required=False)
     nom = fields.Char(string='Nom')
     prenom = fields.Char(string='Prenom')
     numero = fields.Integer(string='Numero')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if 'nom' in vals.keys() or 'prenom' in vals.keys():
+                vals['name'] = f"{vals.get('nom', '')} {vals.get('prenom', '')}"
+
+        return super().create(vals_list)
 
     @api.constrains("nom", "prenom")
     def force_name(self):
