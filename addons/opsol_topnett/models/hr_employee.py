3fe4da4c81502ff1e1ebd54e67e101a9b4aa2134
zaza121 <hamza.yaya121@gmail.com>
2023-10-10 08:09:38 +0100

    [FIX] fix name computation hide main field name and force computation using nom and prenom

--- addons/opsol_topnett/models/hr_employee.py
+++ addons/opsol_topnett/models/hr_employee.py
@@ -5,26 +5,11 @@ from odoo import api, fields, models
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
-    name = fields.Char(
-        compute="_compute_name",
-        inverse="inverse_compute_name"
-    )
     nom = fields.Char(string='Nom')
     prenom = fields.Char(string='Prenom')
     numero = fields.Integer(string='Numero')
 
-    @api.depends("nom", "prenom")
-    def _compute_name(self):
-        for res in self:
-            rec.name = f"{rec.nom} {rec.prenom}"
-
-    @api.onchange('name')
-    def inverse_compute_name(self):
+    @api.constrains("nom", "prenom")
+    def force_name(self):
         for rec in self:
-            values = rec.name and rec.name.split(" ") or []
-            if len(values) > 1:
-                rec.nom = values[0]
-                rec.prenom = ' '.join(values[1:])
-            else:
-                rec.nom = rec.name
-                rec.prenom = ""
+            rec.name = f"{rec.nom} {rec.prenom}"
