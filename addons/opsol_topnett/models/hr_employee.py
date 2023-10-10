43c9f7aa330c7ef4c2fa3f97f2958f961c0f8c37
zaza121 <hamza.yaya121@gmail.com>
2023-10-10 08:44:27 +0100

    [FIX] error during importation

--- addons/opsol_topnett/models/hr_employee.py
+++ addons/opsol_topnett/models/hr_employee.py
@@ -5,6 +5,7 @@ from odoo import api, fields, models
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
+    name = fields.Char(required=False)
     nom = fields.Char(string='Nom')
     prenom = fields.Char(string='Prenom')
     numero = fields.Integer(string='Numero')
@@ -12,4 +13,4 @@ class HrEmployee(models.Model):
     @api.constrains("nom", "prenom")
     def force_name(self):
         for rec in self:
-            rec.name = f"{rec.nom} {rec.prenom}"
+            rec.name = f"{rec.nom or ''} {rec.prenom or ''}"
